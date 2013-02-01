4fb2bebb6f6fd27c5311e06367727fcca4ba1579
Emile Silvis <emile@silvis.co.za>
2013-02-01 07:55:32 +0200

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: ruby obscenity.rb -e "production" -p $PORT
---
+++ obscenity.rb
@@ -0,0 +1,39 @@
+require 'sinatra'
+require 'obscenity'
+require 'json'
+require 'open-uri'
+
+configure do
+	enable :sessions
+end
+
+before do
+	Obscenity.configure do |config|
+	  	config.replacement = :stars
+	  	unless session[:blacklist].empty?
+	  		config.blacklist = session[:blacklist]
+	  	end
+	end
+end
+
+get '/check/:sentence' do	
+	return status, {'Content-Type' => 'application/json'}, {:profane => Obscenity.profane?(params[:sentence]) }.to_json
+end
+
+get '/clean/:sentence' do	
+	return status, {'Content-Type' => 'application/json'}, {:cleaned_sentence => Obscenity.sanitize(params[:sentence])}.to_json
+end
+
+get '/blacklist' do
+	return status, {'Content-Type' => 'text/yaml'}, session[:blacklist].to_yaml
+end
+
+get '/blacklist/:word' do
+	session[:blacklist].push(params[:word])
+	return status, {'Content-Type' => 'text/yaml'}, session[:blacklist].to_yaml
+end
+
+get '/cleanlist' do
+	session[:blacklist] = []
+	return status
+end
